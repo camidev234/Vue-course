cbc080a7bfa2e81c84ac258c16bf2cc0c123e9b7
Camilo Acevedo <camilo222andresace@gmail.com>
2023-12-19 23:36:33 -0500

    Event click

--- vue-course/src/components/StyleBind.vue
+++ vue-course/src/components/StyleBind.vue
@@ -1,18 +1,31 @@
 <template>
-    <p :style="{
-        backgroundColor : 'green',
-        color: 'white'
-    }">Message submit succesfully</p>
+  <p
+    :style="{
+      backgroundColor: 'green',
+      color,
+    }"
+  >
+    Message submit succesfully
+  </p>
+  <button @click="handleClick">Change color</button>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "StyleBind",
+  setup() {
+    let color = ref("#fff");
+    const handleClick = () => (color.value = "black");
     
+    return { color, handleClick };
+  },
+});
 </script>
 
 <style scoped>
-
 .danger {
-    color: red;
+  color: red;
 }
-
 </style>
